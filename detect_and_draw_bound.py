57f2378eb51dbd4fbca895ce550a71a4a767dbac
Daniel Torres <84144166+dtorre38@users.noreply.github.com>
2024-02-20 16:16:25 -0600

    Minor changes.

---
+++ detect_and_draw_bound.py
@@ -0,0 +1,73 @@
+import os
+import cv2
+import numpy as np
+
+
+debug_opencv = 0  # set to 1 for saving opencv images for debugging purposes
+
+
+def detect_and_draw_bound(frame, width=640, height=480):
+    global bounding_box, dx, dy
+    
+    # Initialize bounding box coordinates with default values
+    x, y, w, h = 0, 0, 0, 0
+    
+    # Reshape mujoco frame [height x width, 1] to 3D array for opencv input [height, width, 3]
+    frame_reshaped = frame.reshape((height, width, 3))
+
+    # convert frame to BGR
+    frame_bgr = cv2.cvtColor(frame_reshaped, cv2.COLOR_RGB2BGR)
+
+    # Convert frame to HSV (Hue, Saturation, Value) color space for easier color detection
+    hsv_frame = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2HSV)
+
+    # Define range for color in HSV (link helps define range of values for colors)
+    # https://stackoverflow.com/questions/47483951/how-can-i-define-a-threshold-value-to-detect-only-green-colour-objects-in-an-ima/47483966#47483966
+    lower_color = np.array([20, 0, 0])  # yellow lower
+    upper_color = np.array([35, 255, 255])  # yellow upper
+
+    # Create a mask for detecting color objects in the frame
+    color_mask = cv2.inRange(hsv_frame, lower_color, upper_color)
+
+    # Find contours in the mask
+    contours, _ = cv2.findContours(color_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        # Filter out small contours that may be noise
+        if cv2.contourArea(contour) > 500:
+            # Get bounding box for each yellow object
+            x, y, w, h = cv2.boundingRect(contour)
+
+            # Draw a green bounding box around the yellow object
+            cv2.rectangle(frame_bgr, (x, y), (x+w, y+h), (0, 255, 0), 2)       
+        
+    frame_bdbox = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+    frame_boundbox = frame_bdbox.flatten()
+    
+    bounding_box = np.array([x, y, x+w, y+h])
+    
+    # center of the bounding box
+    bb_center_x = (bounding_box[0] + bounding_box[2]) / 2
+    bb_center_y = (bounding_box[1] + bounding_box[3]) / 2
+
+    # center of the frame
+    frame_center_x = width / 2
+    frame_center_y = height / 2
+
+    # difference between the centers
+    dx = frame_center_x - bb_center_x
+    dy = frame_center_y - bb_center_y
+    
+    if debug_opencv:
+        dirname = os.path.dirname(__file__)
+        
+        filename = os.path.join(dirname, 'test_hsvframe.png')
+        cv2.imwrite(filename, cv2.flip(hsv_frame, -1))
+
+        filename = os.path.join(dirname, 'test_rgb_filtered.png')
+        cv2.imwrite(filename, cv2.flip(frame_bgr, -1))
+        
+        filename = os.path.join(dirname, 'test_filtered.png')
+        cv2.imwrite(filename, cv2.flip(frame_bdbox, -1))
+    
+    return frame_boundbox, bounding_box, dx, dy
--- diff_drive_tracking.py
+++ diff_drive_tracking.py
@@ -3,15 +3,17 @@ from mujoco.glfw import glfw
 import os
 
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 import matplotlib.pyplot as plt
 
 from detect_and_draw_bound import detect_and_draw_bound
 from get_frame import get_frame
 
-xml_path = 'box.xml'  # xml file (assumes this is in the same folder as this file)
-simend = 100  # simulation time
+
+xml_path = 'differential_drive_car.xml' # xml file (assumes this is in the same folder as this file)
+simend = 60  # simulation time
 print_camera_config = 0  # set to 1 to print camera config
-                         # this is useful for initializing view of the model
+                         # this is useful for initializing view of the model)
 
 # For callback functions
 button_left = False
@@ -28,31 +30,117 @@ dy = 0  # frame center ypos - bounding box center ypos
 bounding_box = np.array([0, 0, 0, 0])  # [x, y, x+w, y+h]: bottom left and top right coordinates
 
 
+def quat2euler(quat_mujoco):
+    # mujoco quat is q0,qx,qy,qz
+    # scipy quat is qx,qy,qz,q0
+    q0 = quat_mujoco[0]
+    qx = quat_mujoco[1]
+    qy = quat_mujoco[2]
+    qz = quat_mujoco[3]
+    quat_scipy = np.array([qx,qy,qz,q0])
+
+    r = R.from_quat(quat_scipy)
+    euler = r.as_euler('xyz', degrees=False)
+
+    return euler
+
+
+def euler2quat(euler):
+    r = R.from_euler('xyz', euler, degrees=False)
+    quat_scipy = r.as_quat()
+    # mujoco quat is q0,qx,qy,qz
+    # scipy quat is qx,qy,qz,q0
+    qx = quat_scipy[0]
+    qy = quat_scipy[1]
+    qz = quat_scipy[2]
+    q0 = quat_scipy[3]
+    quat_mujoco = np.array([q0,qx,qy,qz])
+    
+    return quat_mujoco
+
+
 def init_controller(model, data):
+    # initialize the controller here. This function is called once, in the beginning
     pass
 
 
 def controller(model, data):
+    # put the controller here. This function is called inside the simulation.
+    
     # move desired obstacle to be tracked
-    obst_des = 1
-    data.qfrc_applied[obst_des] = 0.1
+    nbody = 4
+    data.xfrc_applied[nbody][1] = 0.1
     
-    # sigmoid for tracking - decent results, better than manually tuned pd controller
-    # u = 0.35 / (1 + np.exp(-data.qpos[obst_des]))
+    # get position of moving obstacle
+    # x_ref = data.xpos[nbody][0] 
+    # y_ref = data.xpos[nbody][1]
     
-    # if data.qpos[0] < np.pi/2:
-    #     data.ctrl[0] = u
+    # # get x and y position of the site
+    # site_xpos = data.site_xpos[0]
+    # x = site_xpos[0]
+    # y = site_xpos[1]
+
+    # # get the angle of the car
+    # quat = np.array([data.qpos[3],data.qpos[4],data.qpos[5],data.qpos[6]])
+    # euler = quat2euler(quat)
+    # theta = euler[2]
+
+    # # Following moving object with known COM position of the object
+    # # set velocity of left and right wheel
+    # Kp = 0.6
+    # px = 0.5
+    # b = 0.25
+    # r = 0.2
+    # v = Kp*(np.cos(theta)*(x_ref - x) + np.sin(theta)*(y_ref - y))
+    # w = (Kp/px)*(-np.sin(theta)*(x_ref - x) + np.cos(theta)*(y_ref - y))
+    # vel_ref_left = v - b*w
+    # vel_ref_right = v + b*w
+    
+    # data.ctrl[0] = vel_ref_left
+    # data.ctrl[1] = vel_ref_right
+    
+    # b = 0.25  # distance between wheels
+    # r = 0.2  # radius of wheels
+    
+    # # tracking moving object via rotation (aiming to keep object centered with help of bounding box)
+    # K = 0.1
+    # w = -K*dx
+    
+    # if dx != frame_width/2:
+    #     # Calculate wheel velocities for rotating in place
+    #     data.ctrl[0] = w * b / (2 * r)
+    #     data.ctrl[1] = -w * b / (2 * r)
     # else:
-    #     data.ctrl[0] = 0
-        
-    # pd controller to track object
-    if dx != frame_width/2:
-        K = 0.1
-        data.ctrl[0] = K*dx
-    else:
-        data.ctrl[0] = 0
+    #     data.ctrl[0] = 0.0
+    #     data.ctrl[1] = 0.0
     
+    # tracking moving obstacle by following - not working well
+    b = 0.25  # distance between wheels
+    r = 0.2  # radius of wheels
     
+    # Set parameters
+    K_rotate = 0.1  # Proportional gain for rotation
+    K_forward = 0.01  # Proportional gain for forward/backward motion
+    desired_distance = 100  # Desired distance from the object (in pixels)
+
+    # Rotation control
+    w = -K_rotate * dx
+
+    # Forward/backward control
+    object_distance = frame_height - bounding_box[1]
+    error_distance = object_distance - desired_distance
+    v = K_forward * error_distance
+
+    # Calculate wheel velocities
+    left_wheel_velocity = v + w * b / (2 * r)
+    right_wheel_velocity = v - w * b / (2 * r)
+
+    # Update motor control commands
+    data.ctrl[0] = left_wheel_velocity
+    data.ctrl[1] = right_wheel_velocity
+
+    
+
 def keyboard(window, key, scancode, act, mods):
     if act == glfw.PRESS and key == glfw.KEY_BACKSPACE:
         mj.mj_resetData(model, data)
@@ -134,7 +222,7 @@ xml_path = abspath
 
 # MuJoCo data structures
 model = mj.MjModel.from_xml_path(xml_path)  # MuJoCo model
-data = mj.MjData(model)                # MuJoCo data
+data = mj.MjData(model)                     # MuJoCo data
 cam = mj.MjvCamera()                        # Abstract camera
 opt = mj.MjvOption()                        # visualization options
 
@@ -157,39 +245,99 @@ glfw.set_mouse_button_callback(window, mouse_button)
 glfw.set_scroll_callback(window, scroll)
 
 # Example on how to set camera configuration
-# cam.azimuth = 90
-# cam.elevation = -45
-# cam.distance = 2
-# cam.lookat = np.array([0.0, 0.0, 0])
-cam.azimuth = 81 ; cam.elevation = -25 ; cam.distance =  9
+# top view
+# cam.azimuth = 90 ; cam.elevation = -90 ; cam.distance =  25
+# cam.lookat =np.array([ 0.0 , 0.0 , 0.0 ])
+
+# perspective
+cam.azimuth = 90.0 ; cam.elevation = -49 ; cam.distance =  25
 cam.lookat =np.array([ 0.0 , 0.0 , 0.0 ])
 
-#initialize the controller
+# initialize the controller
 init_controller(model,data)
 
-#set the controller
+# set the controller
 mj.set_mjcb_control(controller)
+
+t0 = 0
+# x_ref0, y_ref0 = curve(t0)
+# data.qpos[0] = x_ref0
+# data.qpos[1] = y_ref0
+
+euler_x = 0  # rotation about x-axis
+euler_y = 0  # rotation about y-axis
+
+# change euler_z such that it's orientation is at desired initial pose
+euler_z = 0 #rotation about z-axis
+
+euler = np.array([euler_x,euler_y,euler_z])
+quat = euler2quat(euler)
+data.qpos[3] = quat[0]
+data.qpos[4] = quat[1]
+data.qpos[5] = quat[2]
+data.qpos[6] = quat[3]
+
+t = []  # save time
+x = []  # save x,y coordinates of site
+y = []
+xref = []  # save the reference motion
+yref = []
+error_x = []  # error in tracking
+error_y = []
 
 while not glfw.window_should_close(window):
     time_prev = data.time
 
-    while (data.time - time_prev < 1.0/60.0):
+    while data.time - time_prev < 1.0/60.0:
         mj.mj_step(model, data)
 
-    if (data.time>=simend):
-        break
+    site_xpos = data.site_xpos[0]
+
+    # t.append(data.time)
+    # x.append(site_xpos[0])
+    # y.append(site_xpos[1])
+    # x_ref,y_ref = curve(data.time)
+    # xref.append(x_ref)
+    # yref.append(y_ref)
+    # error_x.append(site_xpos[0]-x_ref)
+    # error_y.append(site_xpos[1]-y_ref)
+
+    # if data.time>=simend:
+    #     plt.figure(1)
+    #     plt.subplot(2, 1, 1)
+    #     plt.plot(t,error_x,'k-')
+    #     plt.ylabel("error x")
+    #     plt.subplot(2, 1, 2)
+    #     plt.plot(t,error_y,'k-')
+    #     plt.ylabel("error y")
+    #     plt.xlabel("t")
+    #     plt.title("plot of error vs time")
+
+    #     plt.figure(2)
+    #     plt.plot(x,y)
+    #     plt.plot(xref,yref,'r-.')
+    #     plt.legend(['meas','ref'])
+    #     plt.gca().set_aspect('equal')
+    #     plt.title("plot of trajectory in x-y plane")
+
+    #     plt.show(block=False)
+    #     plt.pause(20)
+    #     plt.close()
+    #     break
 
     # get framebuffer viewport
     viewport_width, viewport_height = glfw.get_framebuffer_size(
         window)
     viewport = mj.MjrRect(0, 0, viewport_width, viewport_height)
 
-    # print camera configuration (help to initialize the view)
-    if (print_camera_config==1):
+    #print camera configuration (help to initialize the view)
+    if print_camera_config==1:
         print('cam.azimuth =',cam.azimuth,';','cam.elevation =',cam.elevation,';','cam.distance = ',cam.distance)
         print('cam.lookat =np.array([',cam.lookat[0],',',cam.lookat[1],',',cam.lookat[2],'])')
 
     # Update scene and render
+    # opt.frame = 6  # check mjtFrame
+                     # NONE=0,BODY,GEOM,SITE,CAMERA,LIGHT,WORLD
     mj.mjv_updateScene(model, data, opt, None, cam,
                        mj.mjtCatBit.mjCAT_ALL.value, scene)
     mj.mjr_render(viewport, scene, context)
@@ -202,7 +350,7 @@ while not glfw.window_should_close(window):
     
     # render bounding box on inset frame
     mj.mjr_drawPixels(frame_boundbox, None, offscreen_viewport, offscreen_context)
-    
+
     # swap OpenGL buffers (blocking call due to v-sync)
     glfw.swap_buffers(window)
 
